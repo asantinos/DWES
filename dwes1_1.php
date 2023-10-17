1c523224b97e181450bed221afd013c4cac1a17f
Alex <aleexsantos1509@gmail.com>
2023-10-17 12:24:17 +0200

    Initial commit. Created first PHP project.

---
+++ dwes1_1.php
@@ -0,0 +1,23 @@
+<!-- Nuestra primera instrucción en PHP! -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Hellow World!</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        echo "Hellow World"
+        ?>
+    </h1>
+    <p>
+        <?php
+        print "Pinta bien"
+        ?>
+    </p>
+</body>
+
+</html>
